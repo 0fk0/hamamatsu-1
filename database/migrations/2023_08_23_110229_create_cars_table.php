51bb26b079fb95aad0ff5aeeb839c61cc52c1fc0
shizuku <nojiri.shizuku.ci@tut.jp>
2023-08-23 12:10:41 +0900

    car model の作成

---
+++ database/migrations/2023_08_23_110229_create_cars_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * @return  void
+     */
+
+    
+    
+    public function up(): void
+    {
+        // "users"テーブルは存在しなかった場合
+        //if (!Schema::hasTable('cars')) {
+            Schema::create('cars', function (Blueprint $table) {
+                $table->bigIncrements('car_id');
+                $table->text('model');
+                $table->integer('capacity');
+                $table->text('addres');
+                $table->decimal('fee', $precision = 8, $scale = 1);
+                $table->text('name_owner');
+                //$table->foreign('school_id')->references('drivin_school')->on('school_id');
+
+            });
+        //}
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
