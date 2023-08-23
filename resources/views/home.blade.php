8367ced6a5263a347745b4025a3b99178458a550
fk <greeeen_ken41ei39jin@icloud.com>
2023-08-23 11:26:39 +0900

    feat:ホーム画面の作成・ルーティングの調整

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,7 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            トップページ
-        </h2>
-    </x-slot>
+@extends('layouts.layout')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <a href="{{ route('post.index') }}" class="bg-white border-b border-gray-200 p-6 block w-full text-center
-                font-semibold text-gray-800 hover:bg-gray-100 text-decoration-none">
-                    掲示板へ移動
-                </a>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+@section('content')
+    <h1>借りた車で運転練習</h1>
+    <a href="">診断を受けて練習してみる？</a>
+    <a href="">※診断を受けない</a>
+@endsection
