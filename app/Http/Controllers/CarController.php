136954ee0e48f96aa38ce356dd1b58549273d2fe
2118100.ma <2118100.ma@sist.ac.jp>
2023-08-24 13:43:24 +0900

    変更しましたデータベースの取得の仕方を変更しました。

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        // Car モデルからすべての車のデータを取得
+        $cars = Car::all();
+
+        // 取得したデータをビューに渡して一覧表示
+        return view('cars.index', ['cars' => $cars]);
+    }
+
+    public function show($id)
+    {
+        // 特定の車の情報を取得
+        $car = Car::find($id);
+
+        // 詳細情報をビューに渡して表示
+        return view('cars.show', ['car' => $car]);
+    }
+}
