15aaeefbb4f8d51675d21484aeb056f21f05b838
2118100.ma <2118100.ma@sist.ac.jp>
2023-08-24 11:39:22 +0900

    Carのモデル作成を行いました。

---
+++ app/Models/Car.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+
+    //テーブル名
+    protected $table = 'cars';
+
+    //可変項目
+    protected $fillable = ['model', 'capacity', 'address','fee','name_owner','img_path'];
+}
