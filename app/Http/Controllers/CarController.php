2c47f83f7ce63178800d084c596dbe0049cdeefb
2118100.ma <2118100.ma@sist.ac.jp>
2023-08-24 14:46:05 +0900

    ましたた変更を行いました

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -6,21 +6,7 @@ use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         // Car モデルからすべての車のデータを取得
