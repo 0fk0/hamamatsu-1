dfa59b9649ed054988e3919a2f88b103f588da90
fk <greeeen_ken41ei39jin@icloud.com>
2023-08-24 20:31:07 +0900

    fix:確認画面遷移時のパラメータを変更

--- resources/views/q1.blade.php
+++ resources/views/q1.blade.php
@@ -2,10 +2,6 @@
 
 @section('content')
     <h1>車の運転に自信がありますか？</h1>
-    <form action="{{ route('carlist') }}" method="get">
-        <button type="submit" name="yn" value="yes">はい</button>
-    </form>
-    <form action="{{ route('q2') }}" method="get">
-        <button type="submit" name="yn" value="no">いいえ</button>
-    </form>
+    <a href="{{ route('carlist', ['result'=>1]) }}">はい</a>
+    <a href="{{ route('q2') }}">いいえ</a>
 @endsection
