b0a33f92a0add4f1a655888c8f0d75894f86a9b6
2118100.ma <2118100.ma@sist.ac.jp>
2023-08-24 15:49:51 +0900

    hennkousimasita

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace App\Models\Models;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
-
-    //テーブル名
+    // テーブル名
     protected $table = 'cars';
 
-    //可変項目
-    protected $fillable = ['model', 'capacity', 'address','fee','name_owner','img_path'];
+    // 可変項目
+    protected $fillable = ['model', 'capacity', 'address', 'fee', 'name_owner', 'img_path'];
 }
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 
 /*
