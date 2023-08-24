3a8da9c16019471df33a55733d94144b394e6ad4
2118100.ma <2118100.ma@sist.ac.jp>
2023-08-24 19:26:58 +0900

    dekita

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -8,10 +8,20 @@ use Illuminate\Http\Request;
 class CarController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
-        // Car モデルからすべての車のデータを取得
-        $cars = Car::all();
+        // アンケート結果を取得
+        $q1_result = $request->input('q1');
+        $q2_result = $request->input('q2');
+
+        // アンケート結果に応じて表示する車のデータを選択
+        if ($q2_result === 'yes') {
+            $cars = Car::where('capacity', '<', 10)->get();
+        } elseif ($q2_result === 'yes') {
+            $cars = Car::where('capacity', '>=', 10)->get();
+        } else {
+            $cars = Car::all();
+        }
 
         // 取得したデータをビューに渡して一覧表示
         return view('carlist', ['cars' => $cars]);
